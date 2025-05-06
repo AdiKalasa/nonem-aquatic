ff3e7c00125e829a5b9dcf87958de470488722c9
Ganti nama <twpthegreat777@gmail.com>
2025-05-06 19:32:02 +0700

    bantu woi

--- public/database.js
+++ public/database.js
@@ -1,4 +1,8 @@
-let runningTeks = "<h2>Spesialis Ikan Hias sejak 2024</h2> <p>Toko Ikan Hias kami didirikan dengan tujuan menyediakan ikan hias berkualitas tinggi kepada para pecinta ikan di seluruh dunia.</p> <p>Kami berkomitmen untuk memberikan pelayanan terbaik kepada pelanggan kami dan menjaga keberlanjutan lingkungan.</p>"
+let runningTeks = "\
+    <h2>Spesialis Ikan Hias sejak 2024</h2> \
+    <p>Toko Ikan Hias kami didirikan dengan tujuan menyediakan ikan hias berkualitas tinggi kepada para pecinta ikan di seluruh dunia. Kami berkomitmen untuk memberikan pelayanan terbaik kepada pelanggan kami dan menjaga keberlanjutan lingkungan.</p> \
+    <p>Kami menawarkan berbagai macam spesies ikan hias, mulai dari ikan air tawar hingga ikan air laut, serta perlengkapan dan makanan ikan berkualitas tinggi. Tim ahli kami siap membantu Anda dalam memilih ikan yang sesuai dengan kebutuhan dan preferensi Anda.</p>\
+    <p>Kami juga aktif dalam kegiatan konservasi lingkungan dan pembudidayaan ikan yang bertanggung jawab. Dengan setiap pembelian, Anda ikut berkontribusi dalam pelestarian keanekaragaman hayati bawah air.</p>"
 let greetings = "Selamat datang di Toko Ikan Hias Nonem Aquatic, tempat terbaik untuk mendapatkan ikan hias berkualitas tinggi!";
 let TENTANGKAMI = "Tentang kita";
 
